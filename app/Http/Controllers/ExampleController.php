45eb58a23cda1a828da57a8c94f3686bc694649c
unknown <Doodle23@yahoo.com>
2023-01-01 22:53:02 +0900

    first commit

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    public function homepage (){
+        return view('homepage'); 
+    }
+
+    public function aboutPage (){
+        return '<h1>Hey</h1>';
+    }
+
+}
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel ="stylesheet" href ="/main.css"/>
+</head>
+
+<nav>
+<div class="logo">Cat App</div>
+<div id = "inputswrapper">
+<input id ="username"></input>
+<input id ="password"></input>
+</div>
+
+</nav>
+
+<body>
+
+</body>
+</html>
