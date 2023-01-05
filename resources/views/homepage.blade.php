05afa6ce6f85e7b15fb85db61075de2589927a43
unknown <Doodle23@yahoo.com>
2023-01-05 18:38:52 +0900

    added login and logout routes

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,11 +1,7 @@
 <x-layout>
     <nav>
         <div class="logo">Cat App</div>
-        <div id = "inputswrapper">
-        <input id ="username"></input>
-        <input id ="password"></input>
-        <button id = "signinbutton">Sign In</button>
-        </div>
+        
         
         </nav>
         
@@ -17,22 +13,35 @@
             @csrf
             <div class="form-group">
               <label for="username-register" class="text-muted mb-1"><small>Username</small></label>
-              <input name="username" id="username-register" class="form-control" type="text" placeholder="Pick a username" autocomplete="off" />
+              <input value = "{{old('username')}}" name="username" id="username-register" class="form-control" type="text" placeholder="Pick a username" autocomplete="off" />
+            @error('username')
+            <p>{{$message}}</p>
+            @enderror
             </div>
 
             <div class="form-group">
               <label for="email-register" class="text-muted mb-1"><small>Email</small></label>
-              <input name="email" id="email-register" class="form-control" type="text" placeholder="you@example.com" autocomplete="off" />
+              <input  value = "{{old('email')}}" name="email" id="email-register" class="form-control" type="text" placeholder="you@example.com" autocomplete="off" />
+              
+              @error('email')
+              <p>{{$message}}</p>
+              @enderror
             </div>
 
             <div class="form-group">
               <label for="password-register" class="text-muted mb-1"><small>Password</small></label>
               <input name="password" id="password-register" class="form-control" type="password" placeholder="Create a password" />
+              @error('password')
+              <p>{{$message}}</p>
+              @enderror
             </div>
 
             <div class="form-group">
               <label for="password-register-confirm" class="text-muted mb-1"><small>Confirm Password</small></label>
               <input name="password_confirmation" id="password-register-confirm" class="form-control" type="password" placeholder="Confirm password" />
+              @error('password_confirmation')
+              <p>{{$message}}</p>
+              @enderror
             </div>
 
             <button type="submit" class="py-3 mt-4 btn btn-lg btn-success btn-block">Sign up for OurApp</button>
