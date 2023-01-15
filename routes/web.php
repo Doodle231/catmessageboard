5e5578c08d08682af63e88562c4916cebd78e4b6
unknown <Doodle23@yahoo.com>
2023-01-15 21:21:08 +0900

    added abiliy to edit posts from database

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::post('/logout', [UserController::class, 'logout'])->middleware('MustBeLog
 Route::get('/create-post' ,[PostController::class, 'showCreateForm'])->middleware('MustBeLoggedIn'); 
 Route::post('/create-post' ,[PostController::class, 'storeNewPost'])->middleware('MustBeLoggedIn');
 Route::get('/post/{post}' ,[PostController::class, 'viewSinglePost']); 
-
+Route::delete('/post/{post}',[PostController::class,'delete'])->middleware('can:delete,post');
+Route::get('/post/{post}/edit', [PostController::class, 'showEditForm'])->middleware('can:update,post'); 
+Route::put('/post/{post}', [PostController::class, 'actuallyUpdate'])->middleware('can:update,post'); 
 // Profile related routes
 Route::get('/profile/{user:username}', [UserController::class, 'profile']); 
